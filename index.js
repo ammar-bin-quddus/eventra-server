27fe4c6d12c8d87494fb1384f0685cadb04f6ea5
ammar-bin-quddus <ammarcghs372@gmail.com>
2025-07-01 23:23:36 +0600

    cors added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const eventRoutes = require('./routes/eventRoutes');
-const errorHandler = require('./middleware/errorHandler');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const eventRoutes = require("./routes/eventRoutes");
+const errorHandler = require("./middleware/errorHandler");
 
 dotenv.config();
 connectDB();
@@ -12,10 +12,13 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(
+  cors({ origin: ["https://eventra-web.web.app/", "http://localhost:5173/"] })
+);
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/events', eventRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
 
 // Global error handler (must be last)
 app.use(errorHandler);
